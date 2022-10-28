57763b923c1bca78407ac1522a5c808aafcab669
Pedro Luiz da Silva Pereira <39136185+plpereir@users.noreply.github.com>
2022-10-28 00:19:33 -0300

    initial commit

---
+++ src/main/java/com/downloader/youtube4mp4/Youtube4mp4Application.java
@@ -0,0 +1,13 @@
+package com.downloader.youtube4mp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Youtube4mp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Youtube4mp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/downloader/youtube4mp4/Youtube4mp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downloader.youtube4mp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Youtube4mp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
